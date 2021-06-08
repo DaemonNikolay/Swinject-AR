113de8254fc2ea6e58c4f9fb98b150d1eb9d21e6
Nikolay Eckert <nikolay-eckert@yandex.ru>
2021-06-08 08:30:33 +0700

    Init

---
+++ Swinject+AR/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swinject+AR
+//
+//  Created by Nikulux on 08.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
